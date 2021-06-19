bda9e3a6e69e1a6e6b5b38059271183c9a4177fc
startjourney9 <jinyixiao69@gmail.com>
2021-06-19 15:14:10 +0200

    remove async keyword from function

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify";
 
-export default function hello(fastify: FastifyInstance) {
+export default function hello(fastify: FastifyInstance, _, done) {
   fastify.get("/hello", async (_req, reply) => reply.status(200).send({ hello: "world" }));
+
+  done();
 }
